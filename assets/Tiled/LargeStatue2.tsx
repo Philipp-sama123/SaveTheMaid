3dc75f1c2a8958f98a9a8ed38634f75fc459be05
philippstampfer <philipp.stampfer@protonmail.ch>
2025-03-09 00:37:45 +0100

    feat - add statue

---
+++ assets/Tiled/LargeStatue2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="LargeStatue2" tilewidth="96" tileheight="160" tilecount="1" columns="1">
+ <image source="../GandalfHardcore Hell Asset Pack 32x32/Large Statue 2.png" width="96" height="160"/>
+</tileset>
